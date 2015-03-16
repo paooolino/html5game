4d16f77bd784782131495c493deab4979b11d56c
Paolo Savoldi <paooolino@gmail.com>
2015-03-16 15:54:29 +0100

    comments

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
 function Game() {
 	
+	// triggered by preload_complete().
 	var startGame = function() {
 		//
 	}
 	
+	// triggered at object creation.
+	// triggers preload_complete().
 	var preload = function() {
 		var loadQueue = new createjs.LoadQueue();
 		loadQueue.addEventListener("fileload", preload_complete);
 		loadQueue.loadFile("assets/tile.png");		
 	}
 	
+	// triggered at preload() complete.
+	// triggers startGame().
 	var preload_complete = function() {
 		if( window.console )console.log('preload complete!');
 		startGame();
