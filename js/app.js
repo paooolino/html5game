6a3e81a373f8c14f67158e9d03c75c0a8daec97b
Paolo Savoldi <paooolino@gmail.com>
2015-03-19 16:30:47 +0100

    refactoring

--- js/app.js
+++ js/app.js
@@ -1,27 +1,8 @@
-function Game() {
-	
-	// triggered by preload_complete().
-	var startGame = function() {
-		//
-	}
-	
-	// triggered at object creation.
-	// triggers preload_complete().
-	var preload = function() {
-		var loadQueue = new createjs.LoadQueue();
-		loadQueue.addEventListener("fileload", preload_complete);
-		loadQueue.loadFile("assets/tile.png");		
-	}
-	
-	// triggered at preload() complete.
-	// triggers startGame().
-	var preload_complete = function() {
-		if( window.console )console.log('preload complete!');
-		startGame();
-	}
-	
-	preload();
-}
+var hs = [
+	0,0,0,0,0,0,
+	1,1,1,1,1,1,
+	0,0,1,1,0,2,
+	2,2,2,1,1,1,
+	1,0,0,0,0,1
+];
 
-if( window.console )console.log('instantiating global object');
-var Game = new Game();
